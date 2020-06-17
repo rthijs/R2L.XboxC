88a396f0e4782d9c256166f550531223e6a8e942
Roel Thijs <roel.thijs@gmail.com>
2020-06-17 14:30:26 +0200

    libgdx project

---
+++ desktop/src/r2l/xboxc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package r2l.xboxc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import r2l.xboxc.XboxControllerInput;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new XboxControllerInput(), config);
+	}
+}
