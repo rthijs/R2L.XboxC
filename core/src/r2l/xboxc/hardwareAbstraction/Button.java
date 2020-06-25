808eae443d2c05b8937e8965fd85036e7424eb53
Roel Thijs <roel.thijs@gmail.com>
2020-06-25 20:35:41 +0200

    weg met de cache, simpeler is beter

--- core/src/r2l/xboxc/hardwareAbstraction/Button.java
+++ core/src/r2l/xboxc/hardwareAbstraction/Button.java
@@ -2,8 +2,6 @@ package r2l.xboxc.hardwareAbstraction;
 
 import com.badlogic.gdx.controllers.mappings.Xbox;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Stream;
 
 public enum Button {
@@ -13,20 +11,17 @@ public enum Button {
     Y(Xbox.Y),
     UNKNOWN(-1);
 
-    private static final Map<Integer, Button> BY_CODE = new HashMap<>(); //for caching
-
-    static {
-        Stream.of(Button.values()).forEach(button -> BY_CODE.put(button.code, button)); //fill cache
-    }
-
     public final int code;
 
-    private Button(int code) {
+    Button(int code) {
         this.code = code;
     }
 
     public static Button valueOfCode(int code) {
-        return BY_CODE.get(code) == null ? UNKNOWN : BY_CODE.get(code);
+        return Stream.of(Button.values())
+                .filter(button -> code == button.code)
+                .findAny()
+                .orElse(UNKNOWN);
     }
 }
 
