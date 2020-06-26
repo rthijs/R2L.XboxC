5ffa45d9d6a18bb568250243ebcd5e0848f86fa6
Roel Thijs <roel.thijs@gmail.com>
2020-06-26 09:23:57 +0200

    vereenvoudiging XboxControllerListener

--- core/src/r2l/xboxc/XboxControllerListener.java
+++ core/src/r2l/xboxc/XboxControllerListener.java
@@ -2,12 +2,11 @@ package r2l.xboxc;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.controllers.Controller;
-import com.badlogic.gdx.controllers.ControllerListener;
+import com.badlogic.gdx.controllers.ControllerAdapter;
 import com.badlogic.gdx.controllers.PovDirection;
-import com.badlogic.gdx.math.Vector3;
 import r2l.xboxc.hardwareAbstraction.Button;
 
-public class XboxControllerListener  implements ControllerListener  {
+public class XboxControllerListener extends ControllerAdapter {
 
     private final int controllerIndex;
 
@@ -15,15 +14,6 @@ public class XboxControllerListener  implements ControllerListener  {
         this.controllerIndex = controllerIndex;
     }
 
-    @Override
-    public void connected(Controller controller) {
-
-    }
-
-    @Override
-    public void disconnected(Controller controller) {
-
-    }
 
     @Override
     public boolean buttonDown(Controller controller, int buttonCode) {
@@ -43,21 +33,6 @@ public class XboxControllerListener  implements ControllerListener  {
 
     @Override
     public boolean povMoved(Controller controller, int povCode, PovDirection value) {
-        return false;
-    }
-
-    @Override
-    public boolean xSliderMoved(Controller controller, int sliderCode, boolean value) {
-        return false;
-    }
-
-    @Override
-    public boolean ySliderMoved(Controller controller, int sliderCode, boolean value) {
-        return false;
-    }
-
-    @Override
-    public boolean accelerometerMoved(Controller controller, int accelerometerCode, Vector3 value) {
         return false;
     }
 }
