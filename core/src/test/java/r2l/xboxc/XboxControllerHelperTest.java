ec62239d74ab6e5930ccfde59daf228e68f00a8f
Roel vanop de laptop <roel.thijs@gmail.com>
2020-07-02 18:51:17 +0200

    ik suck in testen

--- core/src/test/java/r2l/xboxc/XboxControllerHelperTest.java
+++ core/src/test/java/r2l/xboxc/XboxControllerHelperTest.java
@@ -2,16 +2,11 @@ package r2l.xboxc;
 
 import org.junit.Test;
 
-import java.util.List;
-
-import static org.mockito.Mockito.mock;
-
 public class XboxControllerHelperTest {
 
     @Test
     public void addListenerToEveryController_shouldAddListenerToEveryController() {
-        List mockedList = mock(List.class);
-
-        assert mockedList != null;
+        //TODO
+        assert true;
     }
 }
