d5e0fe04c827f4259db30cdab26f2a163ef832e4
Roel Thijs <roel.thijs@gmail.com>
2020-07-03 13:13:44 +0200

    begin of XboxControllerListenerTest

---
+++ core/src/test/java/r2l/xboxc/XboxControllerListenerTest.java
@@ -0,0 +1,8 @@
+package r2l.xboxc;
+
+public class XboxControllerListenerTest {
+
+    private static final int CONTROLLER_INDEX = 123;
+
+    private XboxControllerListener xboxControllerListener = new XboxControllerListener(CONTROLLER_INDEX);
+}
